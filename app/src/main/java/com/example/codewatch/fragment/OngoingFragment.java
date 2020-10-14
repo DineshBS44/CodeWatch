75f0fb57639bca516a329339fa3d0f90612da005
Dinesh B S <dineshbs444@gmail.com>
2020-10-14 14:57:53 +0530

    Created activity, fragment, rest and model packages!

--- app/src/main/java/com/example/codewatch/fragment/OngoingFragment.java
+++ app/src/main/java/com/example/codewatch/fragment/OngoingFragment.java
@@ -1,4 +1,4 @@
-package com.example.codewatch;
+package com.example.codewatch.fragment;
 
 import android.os.Bundle;
 
@@ -8,7 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class UpcomingFragment extends Fragment {
+import com.example.codewatch.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OngoingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OngoingFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,7 +26,7 @@ public class UpcomingFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public UpcomingFragment() {
+    public OngoingFragment() {
         // Required empty public constructor
     }
 
@@ -29,11 +36,11 @@ public class UpcomingFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment UpcomingFragment.
+     * @return A new instance of fragment OngoingFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static UpcomingFragment newInstance(String param1, String param2) {
-        UpcomingFragment fragment = new UpcomingFragment();
+    public static OngoingFragment newInstance(String param1, String param2) {
+        OngoingFragment fragment = new OngoingFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -54,6 +61,6 @@ public class UpcomingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+        return inflater.inflate(R.layout.fragment_ongoing, container, false);
     }
 }
--- app/src/main/java/com/example/codewatch/fragment/UpcomingFragment.java
+++ app/src/main/java/com/example/codewatch/fragment/UpcomingFragment.java
@@ -1,4 +1,4 @@
-package com.example.codewatch;
+package com.example.codewatch.fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OngoingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class OngoingFragment extends Fragment {
+import com.example.codewatch.R;
+
+public class UpcomingFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +21,7 @@ public class OngoingFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public OngoingFragment() {
+    public UpcomingFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +31,11 @@ public class OngoingFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment OngoingFragment.
+     * @return A new instance of fragment UpcomingFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OngoingFragment newInstance(String param1, String param2) {
-        OngoingFragment fragment = new OngoingFragment();
+    public static UpcomingFragment newInstance(String param1, String param2) {
+        UpcomingFragment fragment = new UpcomingFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +56,6 @@ public class OngoingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ongoing, container, false);
+        return inflater.inflate(R.layout.fragment_upcoming, container, false);
     }
 }
